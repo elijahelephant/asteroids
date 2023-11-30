dedec4ae4e3babbbd7c4aad1fab5a98637889004
elijahelephant <elijahwilliams@ucsb.edu>
2023-11-29 23:35:25 -0800

    changes

--- main.cpp
+++ main.cpp
@@ -1,33 +1,76 @@
 #include <GLUT/glut.h>
+#include <cmath>
 
-// Function to handle window resizing
-void reshape(int width, int height) {
-    glViewport(0, 0, width, height);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
-    glMatrixMode(GL_MODELVIEW);
-}
+// Triangle parameters
+float xPos = 0.0f;
+float yPos = 0.0f;
+float angle = 0.0f;
+float moveSpeed = 0.025f;
 
-// Function to render (display) content
-void display() {
-    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+bool keys[256]; // Array to track key states
+
+void drawTriangle() {
     glClear(GL_COLOR_BUFFER_BIT);
+    glLoadIdentity();
+    glColor3f(0.0f, 0.0f, 0.0f);
+
+    glTranslatef(xPos, yPos, 0.0f);
+    glRotatef(angle, 0.0f, 0.0f, 1.0f); // Rotate around z-axis
+
+    // Define vertices centered around (0, 0) for the triangle
+    glBegin(GL_TRIANGLES);
+    glVertex2f(0.0f, 0.08f);  // Top point
+    glVertex2f(-0.08f, -0.08f);  // Bottom-left point
+    glVertex2f(0.08f, -0.08f);  // Bottom-right point
+    glEnd();
+
     glutSwapBuffers();
 }
 
+void update(int value) {
+    glutPostRedisplay();
+    glutTimerFunc(16, update, 0); // Update at approximately 60 FPS
+
+    float dx = cos(angle * 3.14159 / 180.0f) * moveSpeed;
+    float dy = sin(angle * 3.14159 / 180.0f) * moveSpeed;
+
+    if (keys['w']) {
+        xPos += dx;
+        yPos += dy;
+    }
+    if (keys['s']) {
+        xPos -= dx;
+        yPos -= dy;
+    }
+    if (keys['a']) {
+        angle += 3.0f; // Rotate left
+    }
+    if (keys['d']) {
+        angle -= 3.0f; // Rotate right
+    }
+}
+
+void keyboard(unsigned char key, int x, int y) {
+    keys[key] = true;
+}
+
+void keyboardUp(unsigned char key, int x, int y) {
+    keys[key] = false;
+}
+
 int main(int argc, char** argv) {
-    // Initialize GLUT and create a window
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
-    glutInitWindowSize(800, 600); // Window size: width x height
-    glutCreateWindow("Blank Window");
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(500, 500);
+    glutCreateWindow("Moving and Rotating Triangle");
 
-    // Register callback functions
-    glutReshapeFunc(reshape);
-    glutDisplayFunc(display);
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 
-    // Enter the GLUT event-processing loop
+    glutDisplayFunc(drawTriangle);
+    glutKeyboardFunc(keyboard);
+    glutKeyboardUpFunc(keyboardUp);
+    glutTimerFunc(0, update, 0);
+
     glutMainLoop();
 
     return 0;
