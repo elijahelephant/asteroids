8c5d9f1e4de127b0dcf8f93d4baa178e44ff7ff5
liamlizard <82777456+liamlizard@users.noreply.github.com>
2023-12-07 21:27:15 -0800

    Added leaderboard

--- lib/game.cpp
+++ lib/game.cpp
@@ -3,11 +3,19 @@
 #include "player.h"
 #include <GLUT/glut.h>
 #include <iostream>
-#include <string>
 #include <cmath>
+#include <vector>
+#include <algorithm>
+#include <fstream>
 
 int score = 0;
 int finalScore = 0;
+
+std::vector<PlayerScore> leaderboard;
+const std::string leaderboardFilePath = "lib/highscore.txt";
+
+std::string playerNameInput = "";
+bool enteringName = false;
 
 void drawScore() {
     glColor3f(1.0, 1.0, 1.0);
@@ -31,11 +39,17 @@ void display() {
         }
         glColor3f(1.0, 1.0, 1.0);
         glRasterPos2f(-.25, -0.1); // Position for start message
-        std::string startText = "Press Enter to Start";
-        for (char c : startText) {
-            glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, c);
-        }
+        glColor3f(1.0, 1.0, 1.0);
+        drawText(-0.6, -0.15, "Press 'Enter' then enter your name using the keyboard.");
+        drawText(-.10, -0.2775, playerNameInput);
+        drawText(-0.4, -0.40, "Press 'Enter' to start the game");
+
     } else if (gameOver) {
+
+        loadLeaderboard();
+        updateLeaderboard(finalScore);
+        saveLeaderboard();
+
         // Game over message and quit instruction
         glColor3f(1.0, 1.0, 1.0);
         glRasterPos2f(-0.5, 0.1); // Position for game over text
@@ -49,6 +63,8 @@ void display() {
         for (char c : quitText) {
             glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, c);
         }
+        displayLeaderboard();
+
     } else {
         // Your regular game drawing logic when not game over
         drawPlayer();
@@ -79,13 +95,125 @@ void keyboard(unsigned char key, int x, int y) {
     case ' ':
         fireBullet();
         break;
-    case 13:
-        gameStart = true;
-        glutPostOverlayRedisplay();
+    case 13: // Enter key
+        if (!enteringName) {
+            // Switch to entering name mode
+            enteringName = true;
+        } else {
+            // Submit the name and start the game
+            enteringName = false;
+            gameStart = true;
+        }
+        break;
+    case 8: // Backspace key
+        if (enteringName && !playerNameInput.empty()) {
+            playerNameInput.pop_back();
+            glutPostOverlayRedisplay();
+        }
+        break;
+    default:
+        if (enteringName && isalpha(key) && playerNameInput.length() < 20) {
+            playerNameInput += key;
+            glutPostOverlayRedisplay();
+        }
     }
-
 }
 
 void gameOverScreen(int score) {
     return;
+}
+
+void updateLeaderboard(int score) {
+    // Only update leaderboard if a name has been entered
+    if (!playerNameInput.empty()) {
+        // Check if the player's name is already in the leaderboard
+        auto it = std::find_if(leaderboard.begin(), leaderboard.end(),
+                               [&](const PlayerScore& entry) {
+                                   return entry.playerName == playerNameInput;
+                               });
+
+        if (it != leaderboard.end()) {
+            // Update the existing entry if the new score is higher
+            if (score > it->score) {
+                it->score = score;
+                // Resort the leaderboard after updating the score
+                std::sort(leaderboard.begin(), leaderboard.end(),
+                          [](const PlayerScore& a, const PlayerScore& b) {
+                              return a.score > b.score;
+                          });
+            }
+        } else {
+            // Add a new entry if the player's name is not in the leaderboard
+            leaderboard.emplace_back(playerNameInput, score);
+
+            // Resort the leaderboard after adding the new entry
+            std::sort(leaderboard.begin(), leaderboard.end(),
+                      [](const PlayerScore& a, const PlayerScore& b) {
+                          return a.score > b.score;
+                      });
+
+            // Ensure the leaderboard has at most 5 entries
+            if (leaderboard.size() > 5) {
+                leaderboard.resize(5);
+            }
+        }
+    }
+}
+
+void loadLeaderboard() {
+    leaderboard.clear();
+
+    std::ifstream leaderboardFile(leaderboardFilePath);
+
+    if (leaderboardFile.is_open()) {
+        std::string playerName;
+        int score;
+
+        while (leaderboardFile >> playerName >> score) {
+            leaderboard.emplace_back(playerName, score);
+        }
+
+        leaderboardFile.close();
+    }
+}
+
+void saveLeaderboard() {
+    std::ofstream leaderboardFile(leaderboardFilePath);
+
+    for (const auto& entry : leaderboard) {
+        leaderboardFile << entry.playerName << " " << entry.score << std::endl;
+    }
+
+    leaderboardFile.close();
+}
+
+void displayLeaderboard() {
+    loadLeaderboard();
+
+    glColor3f(1.0, 1.0, 1.0);
+    glRasterPos2f(-0.5, -0.15);
+
+    std::string leaderboardText = "Leaderboard";
+    for (char c : leaderboardText) {
+        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, c);
+    }
+
+    float yPos = -0.25;
+    for (const auto& entry : leaderboard) {
+        glRasterPos2f(-0.5, yPos);
+        std::string entryText = entry.playerName + ": " + std::to_string(entry.score);
+        for (char c : entryText) {
+            glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, c);
+        }
+        yPos -= 0.1;
+    }
+
+    saveLeaderboard();
+}
+
+void drawText(float x, float y, const std::string& text) {
+    glRasterPos2f(x, y);
+    for (char c : text) {
+        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, c);
+    }
 }
